c59edc1322725ab6881eef025bca60645d65d60a
Filipe Resende <166765980+filiperr29@users.noreply.github.com>
2025-12-08 14:26:05 -0300

    Refactor main method for better structure and clarity

--- TP.Grafos/Program.cs
+++ TP.Grafos/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -21,60 +21,68 @@ namespace TP.Grafos
        ser transportado pela rota, levando em conta restrições de infraestrutura e tráfego*/
         static void Main(string[] args)
         {
-            Console.WriteLine("Grafo 1\n" +
-                "Grafo 2\n" +
-                "Grafo 3\n" +
-                "Grafo 4\n" +
-                "Grafo 5\n" +
-                "Grafo 6\n" +
-                "Grafo 7");
-            char opcao1 = char.Parse(Console.ReadLine());
-            IGrafos grafo = LeitorDIMACS.Carregar($"grafos/grafo0{opcao1}.dimacs");
-
-            Console.WriteLine("Grafo montado!");
-            Console.WriteLine($"Vértices: {grafo._quantVertices}");
-            Console.WriteLine($"Arestas: {grafo._quantArestas}");
-
-            Console.WriteLine("1) Qual é o trajeto mais econômico para enviar cargas entre dois centros? (Roteamento de Menor Custo)\n" +
-                "2) Qual é o limite diário de escoamento de mercadorias da empresa? (Capacidade Máxima de Escoamento)\n" +
-                "3) Como interligar todos os centros de distribuição ao menor custo? (Expansão da Rede de Comunicação)\n" +
-                "4) Como planejar manutenções sem conflitos de recurso? (Agendamento de Manutenções sem Conflito)\n" +
-                "5) É possível criar um percurso único de inspeção pelas rotas e centros? (Rota Única de Inspeção)\n");
-            char opcao = char.Parse(Console.ReadLine());
-            switch (opcao)
+            int opcao = 0;
+            do
             {
-                case '1':
-                    break;
-                case '2':
-                    int numVertices = grafo._quantVertices;
-                    int[,] capacidadesInt = new int[numVertices + 1, numVertices + 1];
 
-                    for (int u = 1; u <= numVertices; u++)
-                    {
-                        for (int v = 1; v <= numVertices; v++)
+                Console.WriteLine("===- ESCOLHA UM DOS GRAFOS -===" +
+                    "Grafo 1\n" +
+                    "Grafo 2\n" +
+                    "Grafo 3\n" +
+                    "Grafo 4\n" +
+                    "Grafo 5\n" +
+                    "Grafo 6\n" +
+                    "Grafo 7");
+                char opcao1 = char.Parse(Console.ReadLine());
+                IGrafos grafo = LeitorDIMACS.Carregar($"grafos/grafo0{opcao1}.dimacs");
+
+                Console.WriteLine("Grafo montado!");
+                Console.WriteLine($"Vértices: {grafo._quantVertices}");
+                Console.WriteLine($"Arestas: {grafo._quantArestas}");
+
+                Console.WriteLine("===- PERGUNTAS -===" +
+                    "1 - Qual é o trajeto mais econômico para enviar cargas entre dois centros? (Roteamento de Menor Custo)\n" +
+                    "2 - Qual é o limite diário de escoamento de mercadorias da empresa? (Capacidade Máxima de Escoamento)\n" +
+                    "3 - Como interligar todos os centros de distribuição ao menor custo? (Expansão da Rede de Comunicação)\n" +
+                    "4 - Como planejar manutenções sem conflitos de recurso? (Agendamento de Manutenções sem Conflito)\n" +
+                    "5 - É possível criar um percurso único de inspeção pelas rotas e centros? (Rota Única de Inspeção)\n");
+                opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        int numVertices = grafo._quantVertices;
+                        int[,] capacidadesInt = new int[numVertices + 1, numVertices + 1];
+
+                        for (int u = 1; u <= numVertices; u++)
                         {
-                            capacidadesInt[u, v] = (int)grafo.ObterCapacidade(u, v);
+                            for (int v = 1; v <= numVertices; v++)
+                            {
+                                capacidadesInt[u, v] = (int)grafo.ObterCapacidade(u, v);
+                            }
                         }
-                    }
-                    int fonteS = 1;
-                    int sumidouroT = numVertices;
+                        int fonteS = 1;
+                        int sumidouroT = numVertices;
 
-                    GrafoLogistico logistico = new GrafoLogistico(numVertices, capacidadesInt);
+                        GrafoLogistico logistico = new GrafoLogistico(numVertices, capacidadesInt);
 
-                    (int fluxoMaximo, List<(int u, int v)> corteMinimo) = logistico.CalcularFluxoMaximo(fonteS, sumidouroT);
+                        (int fluxoMaximo, List<(int u, int v)> corteMinimo) = logistico.CalcularFluxoMaximo(fonteS, sumidouroT);
 
-                    Console.WriteLine(logistico.FormatarResultadoII(fonteS, sumidouroT, fluxoMaximo, corteMinimo));
-                    break;
-                case '3':
-                    AGM.ExibirResultados(AGM.CalcularAGM(grafo));
-                    break;
-                case '4':
-                    AgendamentoManutencao.ExibirResultados(AgendamentoManutencao.CalcularAgendamento(grafo));
-                    break;
-                case '5':
-                    break;
-            }
+                        Console.WriteLine(logistico.FormatarResultadoII(fonteS, sumidouroT, fluxoMaximo, corteMinimo));
+                        break;
+                    case '3':
+                        AGM.ExibirResultados(AGM.CalcularAGM(grafo));
+                        break;
+                    case '4':
+                        AgendamentoManutencao.ExibirResultados(AgendamentoManutencao.CalcularAgendamento(grafo));
+                        break;
+                    case '5':
+                        break;
+                    default:
+                        Console.WriteLine("Muito Obrigado);
+                }
+            } while (opcao != 0);
         }
-
     }
 }
