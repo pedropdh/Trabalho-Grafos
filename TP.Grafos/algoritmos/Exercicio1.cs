11fd55cdc401266759234b3b037cc8a08da948bd
Pedro N Mendes <1524108@sga.pucminas.br>
2025-12-08 13:21:21 -0300

    alterando

---
+++ TP.Grafos/algoritmos/Exercicio1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP.Grafos.algoritmos
+{
+    internal class Exercicio1
+    {
+    }
+}
